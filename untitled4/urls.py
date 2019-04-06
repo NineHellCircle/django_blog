c394736698f3be7a1dec4a66bbaa5b1715022a00
ninethell <arthack00@gmail.com>
2019-04-06 14:18:48 +0300

    rfrv

--- untitled4/urls.py
+++ untitled4/urls.py
@@ -15,8 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+import helloworld
+from helloworld import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('helloworld.urls')),
+
+    path('register/', views.RegisterFormView.as_view()),
+    path('logout/', views.LogoutView.as_view()),
+    path('login/', views.LoginFormView.as_view()),
 ]
